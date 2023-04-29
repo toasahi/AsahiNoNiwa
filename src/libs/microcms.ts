dc53a37aba52114d08bd53291d21f52dcb204d4d
toasahi <asa1313to@gmail.com>
2023-04-29 11:20:35 +0900

    Micro CMSを追加

---
+++ src/libs/microcms.ts
@@ -0,0 +1,10 @@
+import { MicroCMSQueries, createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: import.meta.env.SECRET_SERVICE_DOMAIN,  
+  apiKey: import.meta.env.SECRET_API_KEY,
+});
+
+export const getBlogs =async (queries:MicroCMSQueries) => {
+  return await client.get({endpoint:'blogs',queries:queries});
+}
