e39d01af79cb449dfe8110b11c87853db5a645d6
toasahi <asa1313to@gmail.com>
2023-06-22 22:46:22 +0900

    seo対策を追加

---
+++ .netlify/functions-internal/chunks/_blogId_@_@astro.ef500367.mjs
@@ -0,0 +1,8 @@
+export { renderers } from "../renderers.mjs";
+import "./astro.80953456.mjs";
+import "react";
+import "react-dom/server";
+
+const page = () => import("./pages/_blogId_.astro.adf4acc2.mjs").then((n) => n._);
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index@_@astro.c5385a4c.mjs
@@ -0,0 +1,8 @@
+export { renderers } from "../renderers.mjs";
+import "./astro.80953456.mjs";
+import "react";
+import "react-dom/server";
+
+const page = () => import("./pages/index.astro.94760fde.mjs");
+
+export { page };
