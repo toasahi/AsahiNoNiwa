06e84cefdb0e2524dc28815bce98d81221f982ea
toasahi <asa1313to@gmail.com>
2023-06-22 22:21:26 +0900

    change import netlify name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
